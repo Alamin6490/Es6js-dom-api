fa4545ec6caddef228588d591bbe6eda6856ee8e
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2025-06-13 12:09:18 +0600

    Initial commit

---
+++ var-let-const.js
@@ -0,0 +1,8 @@
+// var : no reason to use 
+//let : allow it to reassign
+// const : donot allow it to reassign
+const money = 25;
+money = 20; -------->  donot allow it to reassign we are get error 
+<-----but another way we can use value add_--->
+const rich = money + 50;
+console.log(rich);
