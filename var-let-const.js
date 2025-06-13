411b06f1756158f2d77240e43e335d54eb26c69b
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2025-06-13 12:13:02 +0600

    Initial commit

--- var-let-const.js
+++ var-let-const.js
@@ -6,3 +6,8 @@ money = 20; -------->  donot allow it to reassign we are get error
 <-----but another way we can use value add_--->
 const rich = money + 50;
 console.log(rich);
+
+// how can use let keyword//
+let count = 10;
+count = count + 20;
+console.log(count);
